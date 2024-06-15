5d22e38c2afaebf863a0fc81dedcd9fcebf03b51
yeeecheng <yichengliao1003@gmail.com>
2024-06-16 04:47:34 +0800

    add gaussian blur

--- main.cu
+++ main.cu
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "BMP.h"
 
-void Gray(BMP* bmp){
+BMP* Gray(BMP* bmp){
 
     BMP* gray_bmp = (BMP*)malloc(sizeof(BMP));
     gray_bmp->header = bmp->header;
@@ -27,13 +28,81 @@ void Gray(BMP* bmp){
         // 0.299 * R + 0.587 * G + 0.114 * B;
         data[i / 3] = (int)(0.299 * (float)bmp->data[i + 2] + 0.587 * (float)bmp->data[i + 1] + 0.114 * (float)bmp->data[i]);
     }
-    // writing in file per pixel
+    // writing in file 
     fwrite(data, 1, (bmp->info_header->img_size / 3), out);
+    gray_bmp->data = data;
+
+    return gray_bmp;
 }
 
+void getGaussianKernel(double** kernel, int size, int sigma){
+    if(size <= 0 || sigma == 0) return;
 
-void GaussianBlur(){
+    double sum = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            kernel[i][j] = (1.0 / (2 * M_PI * sigma * sigma)) * exp(-((i - size / 2) * (i - size / 2) + (j - size / 2) * (j - size / 2)) / (2 * sigma * sigma));
+            sum += kernel[i][j];
+        }
+    }
+    // normalize
+    sum = 1./sum;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            kernel[i][j] *= sum;
+        }
+    }    
+}
 
+BMP* GaussianBlur(BMP* bmp, int size, double sigma){
+    
+    BMP* gaussian_blur_bmp = (BMP*)malloc(sizeof(BMP));
+    gaussian_blur_bmp->header = bmp->header;
+    gaussian_blur_bmp->info_header = bmp->info_header;
+    gaussian_blur_bmp->rgb_quad = bmp->rgb_quad;
+
+    FILE* out = fopen("./gaussianBlur.bmp", "wb");
+    fwrite(gaussian_blur_bmp->header, sizeof(HEADER), 1, out);
+    fwrite(gaussian_blur_bmp->info_header, sizeof(INFO_HEADER), 1, out);
+    fwrite(gaussian_blur_bmp->rgb_quad, sizeof(RGBQUAD), 256, out);
+    sigma = sigma > 0 ? sigma : ((size - 1) * 0.5 - 1) * 0.3 + 0.8;
+    
+    // create gaussian kernel
+    double** kernel = (double**)malloc(size * sizeof(double*));
+    for(int i = 0; i < size; i++){
+        kernel[i] = (double*)malloc(size * sizeof(double));
+    }
+
+    getGaussianKernel(kernel, size, sigma);
+
+    // raw data mul kernel weight.
+    int width = bmp->info_header->width, height = bmp->info_header->height;
+    unsigned char* data = bmp->data;
+    int m = size / 2;
+    for(int i = m; i < height - m; i++){
+        for(int j = m; j < width - m; j++){
+            
+            double val = 0;
+            for(int ii = -m; ii < m; ii++){
+                for(int jj = -m; jj < m; jj++){
+                    val += bmp->data[(i + ii) * width + (j + jj)] * kernel[ii + m][jj + m];
+                }
+            }
+            data[i * width + j] = (unsigned char) val;
+        }
+    }
+    
+    // writing in file per pixel
+    fwrite(data, 1, (bmp->info_header->img_size), out);
+    gaussian_blur_bmp->data = data;
+    
+    // free
+    for(int i = 0; i < size; i++){
+        free(kernel[i]);
+    }
+    free(kernel);
+
+    return gaussian_blur_bmp;
 }
 
 int main() {
@@ -54,8 +123,8 @@ int main() {
     
     BMP* bmp = OpenImg("./frame_out/frame1.bmp");
     PrintImgInfo(bmp);
-    Gray(bmp);
-
+    BMP* gray_bmp = Gray(bmp);
+    BMP* gaussian_blur_bmp = GaussianBlur(gray_bmp, 31, 1);
 
     free(bmp);
     
